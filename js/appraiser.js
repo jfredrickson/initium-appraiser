ac3f85eee5a92c227a4a1b73a97210e8ae4ed2d2
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2015-11-14 17:02:49 -0500

    fix weapons.json url for real this time

--- js/appraiser.js
+++ js/appraiser.js
@@ -1,3 +1,6 @@
+---
+---
+
 function populateItemInfo(data) {
   $("#item-info").show();
   $("#item-name").text(data.item.name);
@@ -60,7 +63,7 @@ function parseWeapon(input) {
 
 $("#lookup").click(function () {
   var item = parseWeapon($("#paste").val());
-  var xhr = $.get("data/weapons.json");
+  var xhr = $.get("{{ site.baseurl }}/data/weapons.json");
   xhr.done(function (weapons) {
     var weapon = {};
     weapons.forEach(function (w) {
